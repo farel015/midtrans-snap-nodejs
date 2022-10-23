b9d5814b3f636bf37ddb530379dcc3bc698eb41b
Farel Akbar <86312641+farel015@users.noreply.github.com>
2022-10-23 08:25:42 +0700

    Create samplerequest.js

---
+++ samplerequest.js
@@ -0,0 +1,30 @@
+const midtransClient = require('midtrans-client');
+// Create Snap API instance
+let snap = new midtransClient.Snap({
+        // Set to true if you want Production Environment (accept real transaction).
+        isProduction : false,
+        serverKey : 'YOUR_SERVER_KEY'
+    });
+ 
+let parameter = {
+    "transaction_details": {
+        "order_id": "YOUR-ORDERID-123456",
+        "gross_amount": 10000
+    },
+    "credit_card":{
+        "secure" : true
+    },
+    "customer_details": {
+        "first_name": "farel",
+        "last_name": "akbar",
+        "email": "budi.pra@example.com",
+        "phone": "081234567890"
+    }
+};
+ 
+snap.createTransaction(parameter)
+    .then((transaction)=>{
+        // transaction token
+        let transactionToken = transaction.token;
+        console.log('transactionToken:',transactionToken);
+    })
